cc16307298f8f92ff23901738405dbb1394922ae
menstrualz <127664990+menstrualz@users.noreply.github.com>
2024-02-05 19:45:17 +0200

    Update database_config.py

--- assets/database_config.py
+++ assets/database_config.py
@@ -1,7 +1,7 @@
 db_config = {
-    'host': '141.8.192.46',
-    'user': 'a0858111_Support',
-    'password': 'qwerty123',
-    'db': 'a0858111_Support',
+    'host': '...',
+    'user': '...',
+    'password': '...',
+    'db': '...',
     'autocommit': True,
 }
